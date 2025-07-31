2870bb993e353e2939c65fdeb57684aba6276119
Timo Noko <timonoko@users.noreply.github.com>
2025-07-31 15:12:54 +0300

    ongelma

--- saa2.py
+++ saa2.py
@@ -2,35 +2,43 @@
 import requests as u
 import json,pprint,os,time,datetime,random
 
-a=u.get('https://www.supersaa.fi',timeout=20)
-b=a.text.split('>')
-for b2 in b:
-        for c in b2.split('<'):
-            if c != "" and "{" == c[0]:
-                d=json.loads(c)
-                break
-            break
-d=d['props']['pageProps']['weatherOverview']['forecasts']
-
 suunnat=["Pohjois", "Koillis", "Itä", "Kaakko", "Etelä", "Louna",
           "Länsi", "Luode", "Pohjois"]
 
 def ilmansuunta(x):
     return suunnat[round(x/45)]
 
-def tulos(j):
+def lataus():
+   global d
+   a=u.get('https://www.supersaa.fi',timeout=60)
+   b=a.text.split('>')
+   for b2 in b:
+        for c in b2.split('<'):
+            if c != "" and "{" == c[0]:
+                d=json.loads(c)
+                break
+            break
+   d=d['props']['pageProps']['weatherOverview']['forecasts']
+
+
+def tulos(laitos):
+  try:
+    j=d[1][laitos]
     aa=str(j['temperature'])+"° "+\
               ilmansuunta(j['windDirection'])+" "+\
               str(j['windDirection'])+"° "+\
               str(j['windSpeed'])+"m/s "+\
               j['weatherSymbolText']+"\n"
-    return(aa.encode("utf-8"))
+  except:
+    aa=laitos+' ei toimi'
+  return(aa.encode("utf-8"))
 
 eka=1
 def indeksi():
     global eka
+    lataus()
     eka=(eka+1)%2
     if eka==1:
-       return tulos(d[1]['fmi'])
+       return tulos('fmi')
     else:
-       return tulos(d[1]['foreca'])
+       return tulos('foreca')
